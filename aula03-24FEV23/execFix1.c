7c5a584be1669457716bf0152d616b4b1c30d69b
RochaFCLRenan <renan.rocha@aluno.ifsp.edu.br>
2023-03-08 16:37:27 -0300

    doing class 3 exercises

---
+++ aula03-24FEV23/execFix1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+   int inicialVector[8] = {0};
+   int positiveVector[8] = {0};
+   int negativeVector[8] = {0};
+   int i = 0;
+   int VECTOR_LENGHT = 8;
+   for ( i = 0; i < VECTOR_LENGHT; i++)
+   {
+      printf('Digite o valor da posição %d',i);
+      scanf('%d', &inicialVector[i]);
+   }
+   for ( i = 0; i < VECTOR_LENGHT; i++)
+   {
+      if (inicialVector[i] >= 0)
+      {
+         
+      }
+      
+   }
+
+   
+}
