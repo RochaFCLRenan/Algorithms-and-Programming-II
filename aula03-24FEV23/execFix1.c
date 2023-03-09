c04ea9217a841e1be83a9246f18b9368ec9bcd4d
RochaFCLRenan <renan.rocha@aluno.ifsp.edu.br>
2023-03-09 15:29:44 -0300

    add file from class 3 and fixation exercise 1

--- aula03-24FEV23/execFix1.c
+++ aula03-24FEV23/execFix1.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
 
 int main(){
-   int inicialVector[8] = {0};
+   int inicialVector[8];
    int positiveVector[8] = {0};
    int negativeVector[8] = {0};
-   int i = 0;
+   int i;
    int VECTOR_LENGHT = 8;
-   for ( i = 0; i < VECTOR_LENGHT; i++)
-   {
-      printf('Digite o valor da posição %d',i);
-      scanf('%d', &inicialVector[i]);
+   int posVecIndex = 0;
+   int negVecIndex = 0;
+
+   for ( i = 0; i < VECTOR_LENGHT; i++) {
+
+      printf("Digite o valor da posição %d \n", i+1);
+      scanf("%d%*c", &inicialVector[i]);
+
    }
-   for ( i = 0; i < VECTOR_LENGHT; i++)
-   {
-      if (inicialVector[i] >= 0)
-      {
+   for (i = 0; i < VECTOR_LENGHT; i++) {
+      if (inicialVector[i] >= 0) {
+
+         positiveVector[posVecIndex] = inicialVector[i];
+         posVecIndex++;
+
+      }
+      else {
+
+         negativeVector[negVecIndex] = inicialVector[i];
+         negVecIndex++;
          
       }
-      
    }
+   printf("Vetor inicial: ");
+   for (i = 0; i < VECTOR_LENGHT; i++) {
 
-   
+      printf("%d", inicialVector[i]);
+      printf(" ");
+
+   }
+   printf("\n");
+   printf("vetor negativo: ");
+   for (i = 0; i < VECTOR_LENGHT; i++) {
+
+      printf("%d", negativeVector[i]);
+      printf(" ");
+
+   }
+   printf("\n");
+   printf("vetor positivo: ");
+   for (i = 0; i < VECTOR_LENGHT; i++) {
+
+      printf("%d", positiveVector[i]);
+      printf(" ");
+
+   }
 }
